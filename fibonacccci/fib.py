432eaea7e2bb87846668964d0b6116664cf132e4
TinZarOo <mikitkit1994@gmail.com>
2020-01-27 15:29:52 +0630

    fibonaccci

---
+++ fibonacccci/fib.py
@@ -0,0 +1,42 @@
+# fib.py
+
+import fibonacci
+
+# f = fibonacci.fib
+# f(200)
+
+#print(fibonacci.fib2(200))
+#fibonacci.name()
+fibonacci.hello()
+
+# fibonacci.fib(10000)
+# --------------
+
+from fibonacci import fib, fib2, name
+
+f = fib(100)
+name() 
+
+# f = fib2(500)
+# print(f)
+
+# ----------------
+
+# from fibonacci import *
+# fib(500)
+# print(fib2(500))
+
+# ----------------
+
+# import fibonacci as fi
+
+# fi.fib(500)
+
+# ----------------
+
+# from fibonacci import fib as fibonacci
+# fibonacci(500)
+
+# # https://docs.python.org/3/tutorial/module.html#packages-in-multiple-directories
+
+# >>>Student (Import Folder)
